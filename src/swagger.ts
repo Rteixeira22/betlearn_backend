5776b1f310c777a3982cb2d0ca0d290f299c69ab
Tomás Sousa <tomasrafael@ua.pt>
2025-04-12 09:46:17 +0100

    Add Swagger branch to workflow and update API path in swagger options

--- src/swagger.ts
+++ src/swagger.ts
@@ -24,7 +24,7 @@ const options = {
   swaggerDefinition,
   apis: [
     process.env.NODE_ENV === "production"
-      ? "dist/routes/*Routes.js"
+      ? "betlearn_backend/dist/routes/*Routes.js"
       : "src/routes/*Routes.ts",
   ],
 };
