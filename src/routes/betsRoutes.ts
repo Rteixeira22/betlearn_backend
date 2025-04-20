b6e7a59830f645c2bdfdc78b4c4bcbf69bf867fa
Rteixeira22 <r.teixeira@ua.pt>
2025-04-20 11:24:43 +0100

    Fix route order for bets retrieval and count endpoints for clarity

--- src/routes/betsRoutes.ts
+++ src/routes/betsRoutes.ts
@@ -338,14 +338,14 @@ const router = express.Router();
 const betsController = new BetsController();
 
 // GET Bet Routes
-router.get("/:id", betsController.getBetsByUserId); // Get bets by user ID
 /* router.get("/:id/bets/active", betsController.getActiveUserBets); // Get active user bets
 router.get("/:id/bets/concluded", betsController.getConcludedUserBets); // Get concluded user bets
 router.get("/:id/bets/winning", betsController.getWinningUserBets); // Get winning user bets
 router.get("/:id/bets/losing", betsController.getLosingUserBets); */ // Get losing user bets
 router.get("/last/:id", betsController.getLastUserBets); // Get last bet by user ID
-router.get("/count/:id", betsController.countUserBetsById); // Get count of bets by user ID
 router.get("/count/today", betsController.getBetsByDate); // Get count of today's bets
+router.get("/count/:id", betsController.countUserBetsById); // Get count of bets by user ID
+router.get("/:id", betsController.getBetsByUserId); // Get bets by user ID
 
 // POST Bet Routes
 router.post("/:id_user/:id_championship", betsController.createBet); // Create a new bet
